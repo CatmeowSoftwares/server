ce48e491195b02087ed6c70c96e1b1ca894baf2a
MysticSlime <jorenjacob098@gmail.com>
2026-02-03 01:26:21 +0800

    added enum and idk

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,61 @@
 use std::{f32::consts::PI, io::Error, os::windows::process, sync::Arc, time::Duration};
 
-use tokio::{io::{AsyncReadExt, AsyncWriteExt}, net::{self, TcpListener, TcpSocket, TcpStream, UdpSocket}, sync::{Mutex, broadcast::{self, Receiver, Sender}}, time::sleep};
+use num_enum::*;
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::{self, TcpListener, TcpSocket, TcpStream, UdpSocket},
+    sync::{
+        Mutex,
+        broadcast::{self, Receiver, Sender},
+    },
+    time::sleep,
+};
 
+#[derive(IntoPrimitive, TryFromPrimitive)]
+#[repr(u8)]
+enum RequestType {
+    ChatSend = 0,
+    ChatRemove,
+    GetPlayerPos,
+    SetPlayerPos,
+    GetPlayers,
+    PlayerJoined,
+    PlayerLeft,
+    PlayerDisconnected,
+    PlayerLogin,
+    PlayerRegister,
+}
 
+struct Player {
+    position: [f32; 2],
+    username: String,
+    id: u8,
+}
 
+impl Player {
+    fn new(username: impl Into<String>) -> Self {
+        let id = 0;
+        Self {
+            position: [0.0, 0.0],
+            username: username.into(),
+            id: id,
+        }
+    }
+}
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    
     let a = TcpListener::bind("127.0.0.1:8080").await?;
     let a2 = Arc::new(Mutex::new(UdpSocket::bind("127.0.0.1:8080").await?));
     let (tx, _) = broadcast::channel::<String>(16);
     let sock = Arc::clone(&a2);
-    let players: Vec::<Player> = Vec::new();
+    let mut players: Vec<Player> = Vec::new();
+
     loop {
-        
         let (mut stream, addr) = a.accept().await?;
 
         let tx = tx.clone();
         let mut rx = tx.subscribe();
-        tokio::spawn(async move { 
+        tokio::spawn(async move {
             println!("Welcome!");
             let (mut reader, mut writer) = stream.split();
             let text = String::new();
@@ -32,9 +69,23 @@ async fn main() -> Result<(), Error> {
                                     println!("bye!");
                                     break;
                                 }
-                                let t = String::from_utf8(r).unwrap();
-                                println!("aaa{t}");
-                                tx.send(t).unwrap();
+                                match RequestType::try_from(r[0]).unwrap() {
+                                    RequestType::ChatSend=> {
+                                        let t = String::from_utf8(r).unwrap();
+                                        tx.send(t).unwrap();
+                                    },
+                                    RequestType::PlayerLogin=>todo!(),
+                                    RequestType::ChatRemove=>todo!(),
+                                    RequestType::GetPlayerPos=> {
+                                        
+                                    },
+                                    RequestType::SetPlayerPos=>todo!(),
+                                    RequestType::PlayerJoined=>todo!(),
+                                    RequestType::PlayerLeft=>todo!(),
+                                    RequestType::PlayerDisconnected=>todo!(),
+                                    RequestType::GetPlayers=>{}
+                                    RequestType::PlayerRegister => todo!(),
+                                }
                             },
                             Err(_) => ()
                         }
@@ -52,14 +103,8 @@ async fn main() -> Result<(), Error> {
 
 
                 }
-        
             }
-            
-
-
-
         });
-
 
         let sock = Arc::clone(&sock);
         tokio::spawn(async move {
@@ -75,19 +120,8 @@ async fn main() -> Result<(), Error> {
                 let data = &v[..bytes];
                 let str_data = String::from_utf8_lossy(&data).to_string();
                 println!("{:?}", str_data);
-                
             }
         });
     }
     Ok(())
-}
-
-
-
-
-
-
-struct Player {
-    username: String,
-    position: [f64; 2],
 }
